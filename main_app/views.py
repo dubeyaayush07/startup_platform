27c3fa91d3e13e89947af6f2247194a351780c00
dubeyaayush07 <dubeyaayush07@gmail.com>
2020-01-17 21:04:11 +0530

    done

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
 from users.models import Profile
 from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
     return render(request, 'main_app/home.html')
 
 
-class StartupListView(ListView):
+class StartupListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
     model = Profile
     template_name = 'main_app/startups.html'
     context_object_name = 'profiles'
 
     def get_queryset(self):
-        return Profile.objects.filter(role='S')
+        user_category = self.request.user.profile.category
+        return Profile.objects.filter(role='S', category=user_category)
+
+
+    def test_func(self):
+        if self.request.user.profile.role == 'M':
+            return True
+        return False
