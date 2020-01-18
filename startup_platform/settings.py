11b9195b40e8ce095cc740fa5e9ee67ecdb219b1
dubeyaayush07 <dubeyaayush07@gmail.com>
2020-01-18 16:15:31 +0530

    startedd chat app

--- startup_platform/settings.py
+++ startup_platform/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'main_app.apps.MainAppConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
